1fbaa025383cba8ef7cd0dcc406d6fb8a7b7a13a
Ayushi@1019 <amundra@uncc.edu>
2022-05-04 22:26:27 -0400

    Uopdated Sreamlit app

---
+++ scripts/cleaning_prepare.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def cleaner():
+    st.title('Data Cleaning and Preparation')
+    st.image("./assets/notebook_img/cleaner1.png")
+    st.image("./assets/notebook_img/cleaner2.png")
+    st.image("./assets/notebook_img/cleaner3.png")
---
+++ scripts/exploratory_analysis.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def explore():
+    st.title('Exploratory Analysis')
+    st.image("./assets/notebook_img/explo1.png")
+    st.image("./assets/notebook_img/explo2.png")
+    st.image("./assets/notebook_img/explo3.png")
+    st.image("./assets/notebook_img/explo4.png")
